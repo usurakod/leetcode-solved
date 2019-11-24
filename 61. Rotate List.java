068dff072901f94226a4ca87e562d728ac066bbc
Uma <33845196+usurakod@users.noreply.github.com>
2019-11-24 10:12:08 -0800

    Update 61. Rotate List.java

--- 61. Rotate List.java
+++ 61. Rotate List.java
@@ -18,9 +18,6 @@ rotate 2 steps to the right: 1->2->0->NULL
 rotate 3 steps to the right: 0->1->2->NULL
 rotate 4 steps to the right: 2->0->1->NULL */
 
-
-
-
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
         ListNode tail = head;
