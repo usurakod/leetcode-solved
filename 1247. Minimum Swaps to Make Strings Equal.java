aa3618c5378e2aea661db857a878d097e95b2336
Uma <33845196+usurakod@users.noreply.github.com>
2019-12-01 00:57:17 -0800

    Create 1247. Minimum Swaps to Make Strings Equal.java

---
+++ 1247. Minimum Swaps to Make Strings Equal.java
@@ -0,0 +1,49 @@
+/* You are given two strings s1 and s2 of equal length consisting of letters "x" and "y" only. Your task is to make these two strings equal to each other. You can swap any two characters that belong to different strings, which means: swap s1[i] and s2[j].
+
+Return the minimum number of swaps required to make s1 and s2 equal, or return -1 if it is impossible to do so.
+
+ 
+
+Example 1:
+
+Input: s1 = "xx", s2 = "yy"
+Output: 1
+Explanation: 
+Swap s1[0] and s2[1], s1 = "yx", s2 = "yx".
+Example 2: 
+
+Input: s1 = "xy", s2 = "yx"
+Output: 2
+Explanation: 
+Swap s1[0] and s2[0], s1 = "yy", s2 = "xx".
+Swap s1[0] and s2[1], s1 = "xy", s2 = "xy".
+Note that you can't swap s1[0] and s1[1] to make s1 equal to "yx", cause we can only swap chars in different strings.
+Example 3:
+
+Input: s1 = "xx", s2 = "xy"
+Output: -1
+Example 4:
+
+Input: s1 = "xxyyxyxyxx", s2 = "xyyxyxxxyx"
+Output: 4
+
+*****************************************************************************************************************************************************/
+
+class Solution {
+   public int minimumSwap(String s1, String s2) {
+        int x = 0, y = 0;
+        for(int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                if (s1.charAt(i) == 'x') {
+                    x++;
+                } else {
+                    y++;
+                }
+            }
+        }
+        if ((x + y)  % 2 == 1) {
+            return -1;
+        }
+        return (x % 2 == 0) ? (x + y) /2 : x /2 + y /2 + 2;
+    }
+}
